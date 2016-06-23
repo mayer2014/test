055e2c49530f1f2144a138c3e368cc0cb6e405c9
mayer <841439703@qq.com>
2016-06-22 19:17:04 -0700

    test

--- github/test.php
+++ github/test.php
@@ -1,4 +1,20 @@
 <?php
-echo 1;
-
+    echo 1;
+    echo 2;
+    echo 3; 
+    /**
+     * Default settings
+     *
+     * @var array
+     */
+    private $defaultSettings = [
+        'httpVersion' => '1.1',
+        'responseChunkSize' => 4096,
+        'outputBuffering' => 'append',
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => false,
+    ];
+    echo 1;
+    echo 2;
+    echo 3;
 ?>
